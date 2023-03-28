166dc35dab3243c10605448f81f176376acf1be9
t.tatiyev <t.tatiyev@trafficwave.kz>
2023-03-29 01:32:43 +0600

    initial commit

---
+++ customFramework/__init__.py
@@ -0,0 +1,41 @@
+import logging.config
+
+from customFramework.api import API
+from webob import Request, Response
+
+from customFramework.settings import LOGGING
+
+logging.config.dictConfig(LOGGING)
+logger = logging.getLogger(__name__)
+
+
+app = API()
+
+
+@app.route(path='/home')
+def home(request: Request):
+    response = Response()
+    response.text = "Hello from the HOME page"
+    return response
+
+
+@app.route(path="/about")
+def about(request: Request):
+    response = Response()
+    response.text = "Hello from the ABOUT page"
+    return response
+
+
+@app.route(path="/say-my-name/{name}")
+def about(request: Request, name):
+    response = Response()
+    response.text = f"Hello {name}, wyd?"
+    return response
+
+
+@app.route("/sum/{num_1:d}/{num_2:d}")
+def sum(request, num_1, num_2):
+    total = int(num_1) + int(num_2)
+    response = Response()
+    response.text = f"{num_1} + {num_2} = {total}"
+    return response
---
+++ customFramework/api.py
@@ -0,0 +1,53 @@
+import logging
+from typing import Optional
+
+from parse import parse
+from webob import Request, Response
+
+
+logger = logging.getLogger(__name__)
+
+
+class API:
+    _routes = {}
+
+    def __call__(self, environ, start_response) -> Response:
+        request = Request(environ=environ)
+
+        response = self.handle_request(request)
+
+        return response(environ, start_response)
+
+    def _get_route_handler(self, route) -> tuple[Optional[callable], Optional[str]]:
+        for path, handler in self._routes.items():
+            parse_result = parse(path, route)
+            if parse_result is not None:
+                return handler, parse_result.named
+
+        return None, None
+
+    @staticmethod
+    def get_404_response() -> Response:
+        response = Response()
+        response.text = 'Route not found'
+        response.status = 404
+        return response
+
+    def handle_request(self, request) -> Response:
+
+        handler, kwargs = self._get_route_handler(request.path)
+        if handler:
+            response: Response = handler(request, **kwargs)
+        else:
+            response = self.get_404_response()
+
+        logger.info(f'{request.method} {response.status} response for {request.path} route')
+        return response
+
+    def route(self, path):
+        """Register route"""
+        def wrapper(route_handler: callable, *args, **kwargs):
+            self._routes[path] = route_handler
+            return route_handler
+
+        return wrapper
