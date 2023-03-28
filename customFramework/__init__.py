f33545ad78a3edc90bf75913232c931e8ac4d66c
t.tatiyev <t.tatiyev@trafficwave.kz>
2023-03-29 01:34:49 +0600

    add args kwargs

--- customFramework/__init__.py
+++ customFramework/__init__.py
@@ -13,28 +13,28 @@ app = API()
 
 
 @app.route(path='/home')
-def home(request: Request):
+def home(request: Request, *args, **kwargs):
     response = Response()
     response.text = "Hello from the HOME page"
     return response
 
 
 @app.route(path="/about")
-def about(request: Request):
+def about(request: Request, *args, **kwargs):
     response = Response()
     response.text = "Hello from the ABOUT page"
     return response
 
 
 @app.route(path="/say-my-name/{name}")
-def about(request: Request, name):
+def about(request: Request, name, *args, **kwargs):
     response = Response()
     response.text = f"Hello {name}, wyd?"
     return response
 
 
 @app.route("/sum/{num_1:d}/{num_2:d}")
-def sum(request, num_1, num_2):
+def sum(request, num_1, num_2, *args, **kwargs):
     total = int(num_1) + int(num_2)
     response = Response()
     response.text = f"{num_1} + {num_2} = {total}"
