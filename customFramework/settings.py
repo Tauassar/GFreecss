c896a14123c183000853b23e2fc9854d4922a1dc
t.tatiyev <t.tatiyev@trafficwave.kz>
2023-03-29 01:32:52 +0600

    configure logging

---
+++ customFramework/settings.py
@@ -0,0 +1,21 @@
+
+LOG_LEVEL = 'DEBUG'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(funcName)s'
+            '%(process)d %(thread)d %(message)s',
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': LOG_LEVEL,
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose',
+        },
+    },
+    'root': {'level': LOG_LEVEL, 'handlers': ['console']},
+}
